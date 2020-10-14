21ff3a5bf54e932fd7f2b5a98df24338d2902425
Hamza <monosoft54@gmail.com>
2020-10-14 10:39:55 +0500

    inital

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="main">
+    <div class="main-bar">
+      <img style="max-width:200px" src="@/assets/img/logo.png" />
+      <div class="navbar">
+        <ul>
+          <li>
+            <a @mouseover="mouseOver" href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">What's New</a>
+          </li>
+          <li>
+            <a href="#">Our Stores</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar">
+        <ul>
+          <li>
+            <a href="#">Sign In</a>
+          </li>
+          <li>
+            <a href="#">Sign Up</a>
+          </li>
+          <li>
+            <a href="#">
+              <img style="max-width:20px" src="@/assets/img/cart.png" alt />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img style="max-width:20px" src="@/assets/img/search.png" alt />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <subBar v-show="active" style="color:white" />
+  </div>
+</template>
+
+<script>
+import subBar from "./sub-bar";
+export default {
+  data() {
+    return {
+      active: false,
+    };
+  },
+  components: {
+    subBar,
+  },
+  methods: {
+    mouseOver: function () {
+      this.active = !this.active;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  background-color: black;
+}
+.main-bar {
+  display: flex;
+  max-width: 1200px;
+  justify-content: space-between;
+  margin: auto;
+}
+
+.navbar {
+  display: flex;
+}
+
+.login {
+  display: flex;
+}
+.span {
+  margin: auto;
+  color: white;
+}
+
+ul {
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+}
+
+li {
+  display: block;
+  margin: auto;
+}
+
+a {
+  font-size: 1rem;
+  color: white;
+  text-decoration: none;
+  padding: 0 1.3rem;
+  margin: auto;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ src/components/Layout/sub-bar.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="main">
+      <div class="main-txt">
+        <h2>EYES</h2>
+        <p>EYEBROW</p>
+        <p>EYELINER</p>
+        <p>EYESHADOW</p>
+        <p>MASCARA</p>
+        <h2>NAILS</h2>
+        <p>POLISH</p>
+        <p>TREATMENT</p>
+      </div>
+      <div class="main-txt">
+        <h2>LIPS</h2>
+        <p>LIP CARE</p>
+        <p>LIPGLOSS</p>
+        <p>LIPLINER</p>
+        <p>LIPSTICK</p>
+        <h2>ACCESSORIES</h2>
+        <p>BRUSHES</p>
+        <p>TOOLS</p>
+      </div>
+      <div class="main-txt">
+        <h2>FACE</h2>
+        <p>BLUSH</p>
+        <p>CONCEALER</p>
+        <p>CONTOUR & HIGHTLIGHT</p>
+        <p>FOUNDATION</p>
+        <p>POWDER</p>
+        <p>PRIME & SET</p>
+      </div>
+      <div class="main-txt">
+        <img src="@/assets/img/Navbar/Lipgloss.webp" />
+        <h3>PRETTY & PLUMP LIPSTICK</h3>
+        <img src="@/assets/img/Navbar/Break.jpg" />
+        <h3>BREAK FREE EYESHADOW PALETTE</h3>
+      </div>
+      <div class="main-txt">
+        <img src="@/assets/img/Navbar/KeepItPlay.webp" />
+        <h3>KEEP IT PLAYFUL EYESHADOW PALETTE</h3>
+        <img src="@/assets/img/Navbar/Makeup.jpg" />
+        <h3>MAKE UP REMOVE WIPES</h3>
+      </div>
+      <div class="main-txt">
+        <img src="@/assets/img/Navbar/Lipstick.webp" />
+        <h3>LIP ATTRACTION LIPSTICK</h3>
+        <img src="@/assets/img/Navbar/Pastel.jpg" />
+        <h3>PASTEL GEL EXTEME SHINE POLISH</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.main {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  max-width: 1200px;
+  margin: auto;
+}
+.main-txt img {
+  padding-top: 30px;
+  max-width: 180px;
+}
+
+.main-txt h3 {
+  text-align: center;
+  font-size: 1rem;
+  padding-top: 1rem;
+}
+
+h2 {
+  font-size: 1rem;
+  padding-top: 1rem;
+  color: #e9008a;
+}
+
+p {
+  font-size: 0.9rem;
+  padding-top: 1rem;
+}
+</style>
